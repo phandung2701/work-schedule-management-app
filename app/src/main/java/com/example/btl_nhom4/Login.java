5feb625253d749833b1c708a7c7b660ade59f0f0
Lê Thanh Sơn <thanhson0514@gmail.com>
2022-05-25 10:30:52 +0700

    Design View for login

---
+++ app/src/main/java/com/example/btl_nhom4/Login.java
@@ -0,0 +1,29 @@
+package com.example.btl_nhom4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+    Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btn_login = findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentHome = new Intent(v.getContext(), MainActivity.class);
+
+                startActivity(intentHome);
+            }
+        });
+    }
+}
