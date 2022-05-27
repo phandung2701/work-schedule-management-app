4d78d2650121d74db6648724c39ff40d626ac0bc
Lê Thanh Sơn <thanhson0514@gmail.com>
2022-05-27 17:36:43 +0700

    Feature bottom navigation view

--- app/src/main/java/com/example/btl_nhom4/MainActivity.java
+++ app/src/main/java/com/example/btl_nhom4/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.btl_nhom4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setSelectedItemId(R.id.bottom_navigation_home);
     }
 }
