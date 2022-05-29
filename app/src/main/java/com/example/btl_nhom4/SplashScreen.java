b72f8edd06e28c96194632e94bb79a972cf3d839
NewBie-profesion <ptd012001@gmail.com>
2022-05-29 09:11:11 +0700

    splash complete

---
+++ app/src/main/java/com/example/btl_nhom4/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.example.btl_nhom4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        imageView = findViewById(R.id.SplashLogo);
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                imageView.animate().rotationBy(360).withEndAction(this).setDuration(10000).setInterpolator(new LinearInterpolator()).start();
+            }
+        };
+        imageView.animate().rotationBy(360).withEndAction(runnable).setDuration(10000).setInterpolator(new LinearInterpolator()).start();
+
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+
+    }
+}
